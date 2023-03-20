b22e501a0a879cfdcaf19770edee57b408a07952
“Swapnapatil9” <swapnapatil629@gmail.com>
2023-03-20 12:05:41 +0530

    Added: Day38 Sequence Problems

---
+++ Sequenceproblem/addTwoDiceNumbers.js
@@ -0,0 +1,6 @@
+// Random function to add two random Dice number from 1 to 6.
+
+let x = Math.floor(1 + Math.random()*6);
+let y = Math.floor(1 + Math.random()*6);
+let sum = x + y;
+console.log(sum);
---
+++ Sequenceproblem/randomSingleDigitNumber.js
@@ -0,0 +1,4 @@
+// Random function to get single digit number.
+
+var x = Math.floor(Math.random()*10);
+console.log(x);
